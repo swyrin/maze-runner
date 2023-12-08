386761689772827fa72c6e4f19f7b4e0724f2d98
Tien Dat Pham <dat20036@gmail.com>
2023-12-08 11:41:35 +0700

    Change the design of Screen class
    - Screen is now forced abstract.

--- src/Engine/UI/Screen.java
+++ src/Engine/UI/Screen.java
@@ -1,18 +1,34 @@
 package Engine.UI;
 
 import Engine.Core.KeyBinding;
+import Engine.Helper.RenderHelper;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * The screen to be shown on the window.
  * Please note this is just the basic screen, extension is ill-advised.
  */
-public class Screen extends JPanel {
+public abstract class Screen extends JPanel {
     /**
      * The parent window handling this.
      */
     private Window parentWindow;
+
+    /**
+     * Desired frame(s) per second.
+     */
+    private final int targetFps;
+
+    public Screen(int targetFps) {
+        this.targetFps = targetFps;
+        this.init();
+    }
+
+    public Screen() {
+        this(60);
+    }
 
     /**
      * Register a key binding.
@@ -49,4 +65,30 @@ public class Screen extends JPanel {
     public void setParentWindow(Window parentWindow) {
         this.parentWindow = parentWindow;
     }
+
+    /**
+     * Please refrain from using this.
+     * @param g the <code>Graphics</code> object to protect
+     */
+    protected void paintComponent(Graphics g) {
+        super.repaint();
+        super.paintComponent(g);
+        render(g);
+        try {
+            Thread.sleep((long) RenderHelper.getRenderDelayForTargetFps(this.targetFps));
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * The render loop. Runs every time a frame is created.
+     * @param g The graphic parameter.
+     */
+    public abstract void render(Graphics g);
+
+    /**
+     * The initiate method. Run only ONCE at class initialization.
+     */
+    public abstract void init();
 }
