c9db81bdbd384c36c8f93c5c99c08ce03bb3dd85
Tien Dat Pham <dat20036@gmail.com>
2023-12-09 09:00:58 +0700

    Add render setting to the engine.

---
+++ src/Engine/RenderSetting.java
@@ -0,0 +1,35 @@
+package Engine;
+
+import java.awt.*;
+
+/**
+ * The render setting for the engine. NOT THE GAME SETTING.
+ */
+public class RenderSetting {
+    /**
+     * Target frames per second.
+     */
+    public static int maxFps = 24;
+
+    /**
+     * Whether to use Hardware Acceleration.
+     * A value of <value>true</value> will set OpenGL as the renderer.
+     * Otherwise, use software rasterization.
+     */
+    public static boolean useHardwareAcceleration = true;
+
+    /**
+     * Whether to use AA.
+     */
+    public static boolean useAntiAliasing = true;
+
+    /**
+     * Whether to use Java's interpolation for rendering.
+     */
+    public static boolean useInterpolation = true;
+
+    /**
+     * What algorithm to use for interpolation.
+     */
+    public static Object interpolationChoice = RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR;
+}
