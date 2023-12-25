2b8f04ebb4184d170a59d071c9d467182a1e5f76
Mai Xuan Thien <daochich2019@gmail.com>
2023-12-25 16:20:10 +0700

    Auto stash before merge of "main" and "origin/main"

--- src/Game/Maze/Image.java
+++ src/Game/Maze/Image.java
@@ -63,71 +63,10 @@ public class Image {
                             break;
                         case ' ':
                             
-                            break;
-                       
+                            break;  
                     }
                 }
             }
-        
-            
         }
     }
     }
-
-=======
-import java.awt.Image;
-import javax.swing.ImageIcon;
-
-public class Image {
-    private String edgebottomleft = "Wall/wall_edge_bottom_left.png";
-    private String edgebottomright = "Wall/wall_edge_bottom_right.png";
-    private String edgetopleft = "Wall/wall_edge_top_left.png";
-    private String edgetopright = "Wall/wall_edge_top_right.png";
-    private String edgeleft = "Wall/wall_edge_left.png";
-    private String edgeright = "Wall/wall_edge_right.png";
-    private String edgetshapebottomright = "Wall/wall_edge_tshape_bottom_right";
-    private String edgetshapebottomleft = "Wall/wall_edge_tshape_bottom_left.png";
-    private String edgetshaperight = "Wall/wall_edge_tshape_right.png";
-    private String edgetshapeleft = "Wall/wall_edge_tshape_left.png";
-    private String edgemidleft = "Wall/wall_edge_mid_left.png";
-    private String edgemidright = "Wall/wall_edge_mid_right.png";
-
-    public void render(Graphics g) {
-        char[][] maze = {
-            {'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'},
-            {'*', 'O', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '*', ' ', ' ', '*'},
-            {'*', ' ', '*', '*', '*', ' ', '*', ' ', '*', ' ', ' ', '*', ' ', ' ', '*'},
-            {'*', ' ', '*', ' ', ' ', ' ', '*', ' ', '*', ' ', ' ', '*', ' ', ' ', '*'},
-            {'*', ' ', ' ', ' ', ' ', ' ', '*', ' ', '*', ' ', ' ', '*', '*', '*', '*'},
-            {'*', ' ', '*', '*', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '*'},
-            {'*', '*', '*', '*', ' ', '*', ' ', ' ', '*', '*', '*', '*', ' ', ' ', '*'},
-            {'*', ' ', ' ', '*', ' ', '*', ' ', ' ', '*', ' ', ' ', '*', ' ', ' ', '*'},
-            {'*', ' ', ' ', '*', ' ', '*', '*', ' ', '*', ' ', '*', '*', ' ', '*', '*'},
-            {'*', ' ', ' ', ' ', ' ', '*', '*', ' ', ' ', ' ', ' ', '*', '*', '*', '*'},
-            {'*', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '*', ' ', ' ', '*', ' ', ' ', '*'},
-            {'*', ' ', '*', '*', ' ', '*', '*', ' ', '*', ' ', ' ', '*', ' ', ' ', '*'},
-            {'*', ' ', '*', '*', ' ', '*', '*', ' ', '*', ' ', ' ', '*', ' ', ' ', '*'},
-            {'*', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '*', ' ', ' ', ' ', ' ', ' ', '*'},
-            {'*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*', '*'}
-        };
-
-        int tileSize = 32;
-
-        for (int i = 0; i < maze.length; i++) {
-            for (int j = 0; j < maze[i].length; j++) {
-                switch (maze[i][j]) {
-                    case '*':
-                        g.drawImage(new ImageIcon(edgetopleft).getImage(), j * tileSize, i * tileSize, null);
-                        break;
-                    case 'O':
-                        g.drawImage(new ImageIcon(edgebottomright).getImage(), j * tileSize, i * tileSize, null);
-                        break;
-                    case ' ':
-                   
-                        break;
-                }
-            }
-        }
-    }
-}
->>>>>>> Stashed changes
