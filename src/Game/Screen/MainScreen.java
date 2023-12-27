658a4a956a3ef3aecbf70bdf133de41b2d000633
Tien Dat Pham <dat20036@gmail.com>
2023-12-27 23:25:58 +0700

    Add rule introduction screen

--- src/Game/Screen/MainScreen.java
+++ src/Game/Screen/MainScreen.java
@@ -18,7 +18,6 @@ public class MainScreen extends Screen {
         this.setLayout(new BorderLayout());
         this.setBackground(Color.black);
 
-
         JPanel buttonsContainer = new JPanel();
 
         buttonsContainer.setOpaque(false);
@@ -26,12 +25,16 @@ public class MainScreen extends Screen {
         buttonsContainer.setLayout(new BoxLayout(buttonsContainer, BoxLayout.Y_AXIS));
 
         StyleButton startButton = new StyleButton("Start Game");
+        StyleButton ruleButton = new StyleButton("Rule");
         StyleButton exitButton = new StyleButton("Exit Game");
 
         startButton.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ruleButton.setAlignmentX(Component.CENTER_ALIGNMENT);
         exitButton.setAlignmentX(Component.CENTER_ALIGNMENT);
 
         startButton.addActionListener(e -> this.getParentWindow().replaceCurrentScreenWith(new GameScreen()));
+
+        ruleButton.addActionListener(e -> this.getParentWindow().replaceCurrentScreenWith(new RuleScreen()));
 
         exitButton.addActionListener(e -> {
             JFrame currentWindow = this.getParentWindow();
@@ -39,6 +42,7 @@ public class MainScreen extends Screen {
         });
 
         buttonsContainer.add(startButton, BorderLayout.SOUTH);
+        buttonsContainer.add(ruleButton, BorderLayout.SOUTH);
         buttonsContainer.add(exitButton, BorderLayout.SOUTH);
 
         this.add(buttonsContainer, BorderLayout.SOUTH);
