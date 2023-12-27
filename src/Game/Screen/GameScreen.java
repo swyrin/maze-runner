a5a4b33f7045c60930e178f18744e2f53d0cd3d5
Tien Dat Pham <dat20036@gmail.com>
2023-12-27 21:49:42 +0700

    Begin filling stuffs to GameScreen

--- src/Game/Screen/GameScreen.java
+++ src/Game/Screen/GameScreen.java
@@ -1,28 +1,86 @@
 package Game.Screen;
 
 import Engine.UI.Screen;
+import Game.Entity.Maze;
+import Game.Entity.Player;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
-import java.io.File;
-import java.util.Objects;
-import java.util.Set;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class GameScreen extends Screen {
-    private Set<String> mapList;
-    private final int currentMapPosition = 0;
+    private final int currentMapNumber;
+    private Maze currentMaze;
+    private Image wallImg, keyImg;
+    private Player player;
+    private int animPictureFrame = 0;
+
+    public GameScreen(int mapNumber) {
+        this.currentMapNumber = mapNumber;
+
+        try {
+            this.wallImg = ImageIO.read(Files.newInputStream(Paths.get("resources/Wall/wall.png")));
+            this.keyImg = ImageIO.read(Files.newInputStream(Paths.get("resources/Key/key.png")));
+        } catch (IOException e) {
+            //
+        }
+    }
+
+    public GameScreen() {
+        this(0);
+    }
 
     @Override
     public void render(Graphics2D g2d) {
+        Maze maze = this.currentMaze;
+        int[][] map = maze.getMazeMatrix();
 
+        g2d.setColor(new Color(135, 205, 246));
+
+        for (int i = 0; i < maze.getHeight(); i++)
+            for (int j = 0; j < maze.getWidth(); j++) {
+                if (map[i][j] == Maze.WALL_CONST) {
+                    g2d.drawImage(wallImg, 10 + j * 28, 10 + i * 28, null);
+                }
+
+                if (map[i][j] == Maze.KEY_CONST) {
+                    g2d.drawImage(keyImg, 10 + j * 28, 10 + i * 28, null);
+                }
+            }
+
+        g2d.drawImage(this.player.getAnimImg("idle", this.animPictureFrame), 10 + player.getX() * 28, 10 + player.getY() * 28, null);
+        this.animPictureFrame += 1;
+        this.animPictureFrame %= 4;
     }
 
     @Override
     public void init() {
-        mapList = Stream.of(Objects.requireNonNull(new File("resources/Map").listFiles()))
-                .filter(file -> !file.isDirectory())
-                .map(File::getName)
-                .collect(Collectors.toSet());
+        this.setBackground(Color.black);
+        this.getParentWindow().setSize(720, 600);
+
+        String mapStr = "";
+
+        // process map file to string.
+        try {
+            Scanner scn = new Scanner(Files.newInputStream(Paths.get("resources/Map/map" + this.currentMapNumber + ".txt")));
+            StringBuilder sb = new StringBuilder();
+            String line;
+            while (scn.hasNextLine()) {
+                line = scn.nextLine();
+                sb.append(line).append('\n');
+            }
+            mapStr = sb.toString();
+        } catch (Exception e) {
+            System.err.println("Error Reading Map File !");
+        }
+        if (mapStr.isEmpty()) {
+            System.err.println("Map is Empty !");
+        }
+
+        this.currentMaze = Maze.setupFromString(mapStr);
+        this.player = new Player(this.currentMaze.getPlayerStartX(), this.currentMaze.getPlayerStartY(), "lizard");
     }
 }
