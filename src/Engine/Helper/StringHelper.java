931d06e1b58dffde78dec2d74cc48f289f02134a
Tien Dat Pham <dat20036@gmail.com>
2023-12-27 21:00:23 +0700

    Add new utility class to The Engine

---
+++ src/Engine/Helper/StringHelper.java
@@ -0,0 +1,9 @@
+
+package Engine.Helper;
+
+public class StringHelper {
+    public static int countLines(String str) {
+        String[] lines = str.split("\r\n|\r|\n");
+        return lines.length;
+    }
+}
