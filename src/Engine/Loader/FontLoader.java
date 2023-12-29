3889e4f02598e0e8e5986c198c798891aecb2747
Tien Dat Pham <dat20036@gmail.com>
2023-12-30 00:05:57 +0700

    Fix the font creation method

--- src/Engine/Loader/FontLoader.java
+++ src/Engine/Loader/FontLoader.java
@@ -7,10 +7,15 @@ import java.io.IOException;
 public class FontLoader {
     public static Font createFont(String name, float size) {
         try {
-            return Font.createFont(
+            Font font = Font.createFont(
                     Font.TRUETYPE_FONT,
                     new File("resources/Font/" + name + ".ttf")
             ).deriveFont(size);
+
+            // https://stackoverflow.com/questions/7265178/jlabel-html-text-ignores-setfont
+            GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(font);
+
+            return font;
         } catch (IOException | FontFormatException e) {
             return Font.getFont(Font.MONOSPACED);
         }
